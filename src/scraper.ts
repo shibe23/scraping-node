d24d4544b5b1cfae72a542e406d9da6c770acf5e
shibe23 <shibe23.info@gmailcom>
2020-01-26 23:55:22 +0900

    メソッド名を変更

--- src/scraper.ts
+++ src/scraper.ts
@@ -4,26 +4,26 @@ import { formatter } from './formatter';
 import * as readline from 'readline';
 
 interface ScraperTypes {
-  getFromStdIn: () => Promise<string>;
+  getURLFromStdIn: () => Promise<string>;
+  getTextFromAPI: (url: string) => Promise<string>;
   start: () => Promise<string>;
-  getFromAPI: (url: string) => Promise<string>;
 }
 
 export const scraper: ScraperTypes = {
-  getFromStdIn() {
+  getURLFromStdIn() {
     console.log('Enter URL');
     const rl = readline.createInterface(process.stdin, process.stdout);
     const line: Promise<string> = new Promise(res => rl.once('line', res));
     return line;
   },
-  async start() {
-    const url = await this.getFromStdIn();
-    const result = await this.getFromAPI(url);
-    const formatedResult = formatter.format(result);
-    return formatedResult;
-  },
-  async getFromAPI(url: string) {
+  async getTextFromAPI(url: string) {
     const res = await apiAdapter.get(url);
     return res.data;
+  },
+  async start() {
+    const url = await this.getURLFromStdIn();
+    const result = await this.getTextFromAPI(url);
+    const formatedResult = formatter.format(result);
+    return formatedResult;
   },
 };
